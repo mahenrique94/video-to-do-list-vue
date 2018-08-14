64900b5f54a189cdeb5016512d3a29e354a33819
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-08-13 21:47:00 -0300

    Enviando para Github

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>To-Do List with Vue</h1>
+        <input v-model.trim="task">
+        <button @click="addTask" type="button">Adicionar</button>
+        <ul>
+            <li v-for="task in tasks" :key="task">{{ task }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ToDoList",
+    data() {
+        return {
+            task: "",
+            tasks: [ "Task 1", "Task 2", "Task 3", "Task 4" ]
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.task)
+            this.task = ""
+        }
+    }
+}
+</script>
+
